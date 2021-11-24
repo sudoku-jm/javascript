af9ac749ade5a3e87d094b968f44aef2a63185b8
sudoku-jm <sudoku8386@gmail.com>
2021-11-24 22:04:01 +0900

    공통 모듈 분리

--- FE초격차/스타벅스/js/main.js
+++ FE초격차/스타벅스/js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur',function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder','');
-});
-
-
 
 /* badge scroll event */
 const badgeEl = document.querySelector('header .badges');
@@ -188,9 +170,3 @@ spyEls.forEach(function(spyEl){
         .setClassToggle(spyEl, 'show')   //토글할 요소, 토글할 클래스 이름 . 요소가 화면에 보이면 show 클래스 추가
         .addTo(new ScrollMagic.Controller());      // 컨트롤러에 장면을 할당(필수!)
 });
-
-
-/* footer */
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();       //2021
