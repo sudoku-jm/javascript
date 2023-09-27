24f2f57fb179e49b90ca4a1ab337dc706b93485a
sudoku-jm <sudoku8386@gmail.com>
2023-09-27 17:04:51 +0900

    코드를 값으로 다루어 표현력 높이기 - go,pipe함수

---
+++ 함수형 프로그래밍과 javascriptEs6+/lib/fx.js
@@ -0,0 +1,32 @@
+
+const log = console.log;
+
+
+//map , filter, reduce 함수 생성
+const map = (f, iter) => {
+    let res = [];
+    for (const a of iter) {
+      res.push(f(a));
+    }
+    return res;
+  };
+  
+  const filter = (f, iter) => {
+    let res = [];
+    for (const a of iter) {
+      if (f(a)) res.push(a);
+    }
+    return res;
+  };
+  
+  const reduce = (f, acc, iter) => {
+    if (!iter) {
+      iter = acc[Symbol.iterator]();
+      acc = iter.next().value;
+    }
+    for (const a of iter) {
+      acc = f(acc, a);
+    }
+    return acc;
+  };
+  
